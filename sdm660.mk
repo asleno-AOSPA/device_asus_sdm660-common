9bf3241af10807d073b70b80a0fa36ecb913a056
Jake Weinstein <jake@aospa.co>
2020-07-06 18:01:59 +0530

    sdm660-common: configs: Do not balance msm_drm and kgsl-3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Signed-off-by: Adithya R <radithya2002@gmail.com>
    Change-Id: I0248e26a7154c689c0d3f11dabe14a1b149353f9

--- sdm660.mk
+++ sdm660.mk
@@ -197,6 +197,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ Balancer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
